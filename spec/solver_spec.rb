fde2461409aaeb3c66fc5ebfa304118216e635a8
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-24 15:06:00 +0300

    Add test to raise StandardError to negative number

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,12 +1,15 @@
 require_relative '../solver'
 
 describe Solver do
-  describe '#factorial' do
-    it 'returns the factorial of the number' do
-      solver = Solver.new
-      expect(solver.factorial(1)).to eq(1)
-      expect(solver.factorial(5)).to eq(120)
-      expect(solver.factorial(0)).to eq(1)
+    describe '#factorial' do
+      solver = Solver.new  
+      it 'should raise an exception when number is negative' do
+        expect{ solver.factorial(-2) }.to raise_error(StandardError, 'Number cannot be negative')
+      end
+      it 'returns the factorial of the number' do
+        expect(solver.factorial(1)).to eq(1)
+        expect(solver.factorial(5)).to eq(120)
+        expect(solver.factorial(0)).to eq(1)
+      end
     end
   end
-end
