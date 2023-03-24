8d19616d0036ae059670ee9f1ea9c82f10297e56
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-24 13:57:45 +0300

    Add a method for  non-negative number

--- solver.rb
+++ solver.rb
@@ -1,6 +1,10 @@
 class Solver
-     
-    def factorial(number)
-        @number = number
+  def factorial(number)
+    answer = 1
+    while number.positive?
+      answer *= number
+      number -= 1
     end
+    answer
+  end
 end
