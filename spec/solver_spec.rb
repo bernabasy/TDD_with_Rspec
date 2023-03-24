554a9c23f3ffd0951d3abff005cb36fcafffd5db
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-24 13:21:59 +0300

    Add a solver class and test for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,11 @@
+require_relative '../solver'
 
 describe Solver do
+  describe '#factorial' do
+ it 'returns the factorial of the number' do
 
-   
+    solver = Solver.new
+    expect(solver.factorial(1)).to eq(1)
+    end
+end
 end
